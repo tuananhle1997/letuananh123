ea08ddc82182ff10cf367bb00582a5ae65746f0c
tuananhle1997 <tuananhle250597@gmail.com>
2019-05-22 07:56:51 +0700

    init

--- index.php
+++ index.php
@@ -10,12 +10,29 @@
 <?php
     $height = $weight = '';
     if (isset($_POST["height"])&& isset($_POST["weight"])){
+        echo "<pre>";
+        print_r($_POST);
+        echo "</pre>";
         $weight = $_POST["weight"];
         $height = $_POST["height"];
 
         $bmi = $weight/( $height*$height);
 
         echo "Chỉ số BMI là: " . $bmi;
+
+        if ($bmi < 18.5){
+            echo "Gầy";
+        }elseif ((18.5 < $bmi) && ($bmi < 24.9)){
+            echo "bình thường";
+        }elseif ((25.0 < $bmi) && ($bmi < 29.9)){
+            echo "Hơi béo";
+        }elseif ((30.0 < $bmi) && ($bmi < 34.9)){
+            echo "Béo phì cấp độ 1";
+        }elseif ((35.0 < $bmi) && ($bmi < 39.9)){
+            echo "Béo phì cấp độ 2";
+        }elseif ($bmi > 40){
+            echo "Béo phì cấp độ 3";
+        }
     }
 
 ?>
